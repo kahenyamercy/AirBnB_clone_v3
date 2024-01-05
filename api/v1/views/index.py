b6c7934728d8165f96272a0c6d98b33d09c83c67
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-05 14:57:38 +0000

    Add test file for get and count storage methods

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@ Module to define all api routes
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
@@ -12,3 +18,24 @@ def status():
     Returns a JSON dictionary
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Returns the number of each object by type
+    """
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    stats = {
+            "amenities": amenities,
+            "cities": cities,
+            "places": places,
+            "reviews": reviews,
+            "states": states,
+            "users": user
+        }
+    return jsonify(stats)
